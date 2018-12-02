1e4efac2cc9dac9cd37495cb2ae8cfee4367547a
Ran Chen <rchen123@umd.edu>
2018-12-02 18:37:52 -0500

    Merge branch 'master' of https://github.com/nisch100/CallABus into ran5
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/nisch100/call_a_bus/Utilities.java
@@ -0,0 +1,19 @@
+package com.example.nisch100.call_a_bus;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class Utilities {
+
+    // Switch to another screen
+    static void switchScreen(Context current, String next){
+        try{
+            Intent intent = new Intent(current, Class.forName(next));
+            current.startActivity(intent);
+        }
+        catch (ClassNotFoundException e) {
+            return;
+        }
+    }
+
+}
