2a7c1433f0f27907b6fd0bb82226990c22e20051
Ran Chen <rchen123@umd.edu>
2018-12-02 16:50:47 -0500

    Merge branch 'master' of https://github.com/nisch100/CallABus into ran5
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/nisch100/call_a_bus/AddRelatives.java
+++ app/src/main/java/com/example/nisch100/call_a_bus/AddRelatives.java
@@ -1,13 +1,17 @@
 package com.example.nisch100.call_a_bus;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -30,12 +34,53 @@ public class AddRelatives extends AppCompatActivity {
     private String password;
     private String number;
     private String name;
+    Relative relativeOne = null;
+    Relative relativeTwo = null;
+    Relative relativeThree = null;
+
+    String rel1Name, rel1Phone, rel2Name, rel2Phone, rel3Name, rel3Phone;
     private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_relatives);
+
+//        email = getIntent().getExtras().getString("email");
+//        password = getIntent().getExtras().getString("password");
+//        name = getIntent().getExtras().getString("name");
+//        number = getIntent().getExtras().getString("number");
+//        signup = (Button) findViewById(R.id.buttonSignup);
+
+        TextView why = (TextView) findViewById(R.id.why);
+        why.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg = "Users may add between one and three relatives. " +
+                        "Users may set relatives to be reminded while scheduling a bus. " +
+                        "Relatives will receive a text message with a scheduled bus' information if selected " +
+                        "by a user when scheduling a bus.";
+                final AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(view.getContext(), android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(getApplicationContext());
+                }
+                builder.setTitle("About Relatives")
+                        .setMessage(msg)
+                        .setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .show();
+                return;
+            }
+        });
+
+
+
         email = getIntent().getExtras().getString("email");
         password = getIntent().getExtras().getString("password");
         name = getIntent().getExtras().getString("name");
@@ -45,12 +90,36 @@ public class AddRelatives extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                rel1Name = ((EditText) findViewById(R.id.relative1Name)).getText().toString().trim();
+                rel1Phone = ((EditText) findViewById(R.id.relative1Phone)).getText().toString().trim();
+
+                if(rel1Name.equals("") || rel1Phone.equals("")){
+                    final AlertDialog.Builder builder;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        builder = new AlertDialog.Builder(view.getContext(), android.R.style.Theme_Material_Dialog_Alert);
+                    } else {
+                        builder = new AlertDialog.Builder(getApplicationContext());
+                    }
+                    builder.setTitle("Error")
+                            .setMessage("You must fill out information for Relative 1.")
+                            .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                    return;
+                } else {
+                    if(!setRelatives()){
+                        Toast.makeText(view.getContext(), "All fields of a relative must be filled if you are trying to add them.", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+
                 progressDialog = new ProgressDialog(AddRelatives.this);
                 progressDialog.setMessage("Registering Please Wait...");
                 progressDialog.show();
-                relName = ((EditText) findViewById(R.id.editTextRelName)).getText().toString().trim();
-                relRel = ((EditText) findViewById(R.id.editTextRelRelationship)).getText().toString().trim();
-                relNumber = ((EditText) findViewById(R.id.editTextRelNumber)).getText().toString().trim();
                 firebaseAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(AddRelatives.this, new OnCompleteListener<AuthResult>() {
                             @Override
@@ -58,7 +127,7 @@ public class AddRelatives extends AppCompatActivity {
                                 //checking if success
                                 if(task.isSuccessful()){
                                     saveUserInfo(task.getResult().getUser().getUid());
-                                    firebaseAuth.signInWithEmailAndPassword(email, password);
+                                    //firebaseAuth.signInWithEmailAndPassword(email, password);
                                     //firebaseAuth.signOut();
                                     finish();
                                     startActivity(new Intent(getApplicationContext(), MainMenu.class));
@@ -67,18 +136,51 @@ public class AddRelatives extends AppCompatActivity {
                                     Toast.makeText(AddRelatives.this,"Registration Error",Toast.LENGTH_LONG).show();
                                 }
                                 progressDialog.dismiss();
+
                             }
                             private void saveUserInfo(String uid){
-                                Registerfirebase obj = new Registerfirebase(name,email,number,password, relName, relRel, relNumber);
+                                Registerfirebase obj = new Registerfirebase(name,email,number,password);
                                 databaseReference = FirebaseDatabase.getInstance().getReference();
                                 //FirebaseUser user = firebaseAuth.getCurrentUser();
                                 //databaseReference.child(user.getUid()).setValue(obj);
                                 databaseReference.child("users").child(uid).setValue(obj);
+
+                                databaseReference.child("relatives").child(uid).child("rel1").setValue(relativeOne);
+                                databaseReference.child("relatives").child(uid).child("rel2").setValue(relativeTwo);
+                                databaseReference.child("relatives").child(uid).child("rel3").setValue(relativeThree);
 
                                 Toast.makeText(AddRelatives.this,"RegisterInfo is saved",Toast.LENGTH_LONG).show();
                             }
                         });
             }
         });
+    }
+
+    public boolean setRelatives() {
+        relativeOne = new Relative(rel1Name, rel1Phone);
+
+        rel2Name = ((EditText) findViewById(R.id.relative2Name)).getText().toString().trim();
+        rel2Phone = ((EditText) findViewById(R.id.relative2Phone)).getText().toString().trim();
+
+        rel3Name = ((EditText) findViewById(R.id.relative3Name)).getText().toString().trim();
+        rel3Phone = ((EditText) findViewById(R.id.relative3Phone)).getText().toString().trim();
+
+        if((!rel2Name.equals("") && rel2Phone.equals("")) || (rel2Name.equals("") && !rel2Phone.equals(""))){
+            return false;
+        } else if (!rel2Name.equals("") && !rel2Phone.equals("")){
+            relativeTwo = new Relative(rel2Name, rel2Phone);
+        }else if (rel2Name.equals("") && rel2Phone.equals("")) {
+            relativeTwo = new Relative("", "");
+        }
+
+        if((!rel3Name.equals("") && rel3Phone.equals("")) || (rel3Name.equals("") && !rel3Phone.equals(""))){
+            return false;
+        } else if (!rel3Name.equals("") && !rel3Phone.equals("")){
+            relativeThree = new Relative(rel3Name, rel3Phone);
+        } else if(rel3Name.equals("") && rel3Phone.equals("")){
+            relativeThree = new Relative("", "");
+        }
+
+        return true;
     }
 }
