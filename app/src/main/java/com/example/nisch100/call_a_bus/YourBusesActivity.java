90116918071080d5c9e224efebab04bbb70583ae
Ran Chen <rchen123@umd.edu>
2018-12-03 20:22:58 -0500

    commit 20:00

--- app/src/main/java/com/example/nisch100/call_a_bus/YourBusesActivity.java
+++ app/src/main/java/com/example/nisch100/call_a_bus/YourBusesActivity.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -80,25 +81,21 @@ public class YourBusesActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    // Switch to Main Menu (upon button click)
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_your_buses, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_general, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.main_menu_button) {
+            // make intent to swithc to main menu
+            Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+            startActivity(intent);
         }
-
         return super.onOptionsItemSelected(item);
     }
 
